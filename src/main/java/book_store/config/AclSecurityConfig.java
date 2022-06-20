a3b3dab98b72f60ae68ae4c08b8390241060b34a
AllEgg <clodpupon@gmail.com>
2022-06-20 11:55:14 +0300

    Added README

--- src/main/java/book_store/config/AclSecurityConfig.java
+++ src/main/java/book_store/config/AclSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.access.expression.method.MethodSecurityExpre
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
+
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true )
 public class AclSecurityConfig extends GlobalMethodSecurityConfiguration {
